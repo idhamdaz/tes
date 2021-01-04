6fa3e1c90fc8cb029ca581a77516ca595a1c8f7e
idhamdaz <76897779+idhamdaz@users.noreply.github.com>
2021-01-04 14:35:03 +0700

    Update tes.js

--- tes.js
+++ tes.js
@@ -1,46 +1,20 @@
-MathJax={
-    loader: {load: ['[tex]/color','[tex]/cancel','[tex]/enclose']},
-    tex: {
-       inlineMath: [['$', '$'], ['\\(', '\\)']],
-       packages: {'[+]': ['color','cancel','enclose']},
-       macros: {
-          degree: "{^\\circ}"
-       }
-    },
-    options:{
-       skipHtmlTags: ["script","noscript","style","textarea","pre","code"]
+var navi = ["/2020/08/exercise-zone-persamaan-nilai-mutlak.html"];
+var pagi = document.getElementsByClassName("pagi");
+if(navi.length>1){
+    document.getElementById("linknav1").classList.replace("d-none","d-block");
+    document.getElementById("linknav2").classList.replace("d-none","d-block");
+    for (var indeks10=0;indeks10<pagi.length;indeks10++){
+        for(var indeksnav=0;indeksnav<navi.length;indeksnav++){
+            pagi[indeks10].innerHTML += "<li class='page-item'><a class='page-link' href='"+navi[indeksnav]+"'>"+(indeksnav+1)+"</a></li>"
+        }
     }
-};
-function jadiin(){
-   var els = document.getElementsByTagName("m");
-for (var i =0; i< els.length; i++) {
- e = els[i]
- e.id = "latex_temp_id"
- katex.render(e.textContent, latex_temp_id);
- e.id = "";
 }
-}
-function jadiin2(){
-   var els = document.getElementsByClassName("m")
-for (var i =0; i< els.length; i++) {
- e = els[i]
- e.id = "latex_temp_id"
- katex.render(e.textContent, latex_temp_id);
- e.id = "";
-}
-}
-
-function kasihin(awal){
-   var banyaksoal = document.getElementsByClassName("nomor");
-   var banyaktombol = document.getElementsByClassName("tbl");
-   var jawaban = document.getElementsByClassName("isi");
-   var tbltutup = document.getElementsByClassName("tutup");
-   for(var indeks=0;indeks<banyaksoal.length;indeks++){
-      banyaksoal[indeks].innerHTML = awal+indeks;
-      banyaktombol[indeks].setAttribute("data-bs-target","#j"+indeks);
-      banyaktombol[indeks].setAttribute("data-bs-toggle","collapse");
-      jawaban[indeks].id = "j"+indeks;
-      tbltutup[indeks].setAttribute("data-bs-target","#j"+indeks);
-      tbltutup[indeks].setAttribute("data-bs-toggle","collapse");
-  };
+function navke(berapa){
+    if(navi.length>1){
+        for(var indeks11=0;indeks11<pagi.length;indeks11++){
+            document.getElementsByClassName("pagi")[indeks11].getElementsByTagName("li")[berapa-1].getElementsByTagName("a")[0].classList.add("disabled");
+            document.getElementsByClassName("pagi")[indeks11].getElementsByTagName("li")[berapa-1].classList.add("active")
+        }
+    }
+    kasihin((berapa-1)*10+1);
 }
