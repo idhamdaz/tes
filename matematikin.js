ea5433e63f44a9b20700358e837bfb947e134459
idhamdaz <76897779+idhamdaz@users.noreply.github.com>
2021-02-20 13:44:22 +0700

    Update matematikin.js

--- matematikin.js
+++ matematikin.js
@@ -5,3 +5,13 @@ function jumlah(...theArgs) {
 function mod(a,b){
  return a%b; 
 }
+  const gcd = (a, b) => a ? gcd(b % a, a) : b;
+  const lcm = (a, b) => a * b / gcd(a, b);
+function kpk(...args) {
+  var keli = args.reduce(lcm);
+  return keli
+}
+function fpb(...args){
+  var fakt = args.reduce(gcd);
+  return fakt;
+}
