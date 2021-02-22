216ea1281616eebdefbdcb4cbcaab3fe1ef7d1b2
idhamdaz <76897779+idhamdaz@users.noreply.github.com>
2021-02-22 14:42:28 +0700

    Update pretessma.js

--- pretessma.js
+++ pretessma.js
@@ -1,9 +1,9 @@
 var myObj = {
 	"soal":
 		{"no1":
-	 		[{"t":"","b":"","s1":"","s2":"","s3":""}],
-		 	[{"t":"","b":"","s1":"","s2":"","s3":""}],
-			[{"t":"","b":"","s1":"","s2":"","s3":""}]
+	 		[{"t":"Urutkan bilangan-bilangan berikut dari yang terkecil hingga terbesar<br/>5, 8, 16, 9","b":"5, 8, 9, 16","s1":"5, 9, 8, 16","s2":"8, 9, 5, 16","s3":"16, 9, 8, 5"}],
+		 	[{"t":"Urutkan bilangan-bilangan berikut dari yang terbesar hingga terkecil<br/>5, 8, 16, 9","b":"16, 9, 8, 5","s1":"5, 8, 9, 16","s2":"5, 9, 8, 16","s3":"8, 9, 5, 16"}],
+			[{"t":"Urutkan bilangan-bilangan berikut dari yang terbesar hingga terkecil<br/>12, 6, 19, 5","b":"19, 12, 6, 5","s1":"5, 6, 12, 19","s2":"12, 19, 5, 6","s3":"12, 5, 6, 19"}]
 		},
 		{"no2":
 	 		[{"t":"","b":"","s1":"","s2":"","s3":""}],
