fd45559329fd21b7f0c24743fd137b6fe4033bab
idhamdaz <76897779+idhamdaz@users.noreply.github.com>
2021-02-23 11:16:30 +0700

    Update pretessma.js

--- pretessma.js
+++ pretessma.js
@@ -6,18 +6,18 @@ var myObj = {
 			{"t":"Urutkan bilangan-bilangan berikut dari yang terbesar hingga terkecil<br/>12, 6, 19, 5","b":"19, 12, 6, 5","s1":"5, 6, 12, 19","s2":"12, 19, 5, 6","s3":"12, 5, 6, 19"}]
 		 },
 		{"no":
-	 		[{"t":"<m>2+19=</m>","b":"<m>21</m>","s1":"20","s2":"22","s3":"23"},
+	 		[{"t":"<m>2+19=</m>","b":"21","s1":"20","s2":"22","s3":"23"},
 		 	{"t":"<m>3+17=</m>","b":"20","s1":"19","s2":"18","s3":"21"},
 			{"t":"<m>19-2=</m>","b":"17","s1":"18","s2":"19","s3":"20"}]
 		 },
 		{"no":
-	 		[{"t":"","b":"","s1":"","s2":"","s3":""},
-		 	{"t":"","b":"","s1":"","s2":"","s3":""},
-			{"t":"","b":"","s1":"","s2":"","s3":""}]
+	 		[{"t":"<m>2\\times16=</m>","b":"32","s1":"31","s2":"30","s3":"29"},
+		 	{"t":"<m>3\\times15=</m>","b":"45","s1":"42","s2":"39","s3":"48"},
+			{"t":"<m>4\\times14=</m>","b":"56","s1":"60","s2":"64","s3":"68"}]
 		 },
 		{"no":
-	 		[{"t":"","b":"","s1":"","s2":"","s3":""},
-		 	{"t":"","b":"","s1":"","s2":"","s3":""},
-			{"t":"","b":"","s1":"","s2":"","s3":""}]
+	 		[{"t":"Urutkan bilangan pecahan berikut dari yang terbesar<br/><m>\\dfrac12</m>, <m>\\dfrac{11}{16}</m>, <m>\\dfrac3{32}</m>, <m>\\dfrac68</m>","b":"<m>\\dfrac68</m>, <m>\\dfrac{11}{16}</m>, <m>\\dfrac12</m>, <m>\\dfrac3{32}</m>","s1":"<m>\\dfrac12</m>, <m>\\dfrac68</m>, <m>\\dfrac{11}{16}</m>, <m>\\dfrac3{32}</m>","s2":"<m>\\dfrac12</m>, <m>\\dfrac{11}{16}</m>, <m>\\dfrac3{32}</m>, <m>\\dfrac68</m>","s3":"<m>\\dfrac3{32}</m>, <m>\\dfrac{11}{16}</m>, <m>\\dfrac68</m>, <m>\\dfrac12</m>"},
+		 	{"t":"Urutkan bilangan pecahan berikut dari yang terkecil<br/><m>\\dfrac7{24}</m>, <m>\\dfrac36</m>, <m>\\dfrac13</m>, <m>\\dfrac38</m>","b":"<m>\\dfrac7{24}</m>, <m>\\dfrac13</m>, <m>\\dfrac38</m>, <m>\\dfrac36</m>","s1":"<m>\\dfrac13</m>, <m>\\dfrac36</m>, <m>\\dfrac38</m>, <m>\\dfrac7{24}</m>","s2":"<m>\\dfrac7{24}</m>, <m>\\dfrac38</m>, <m>\\dfrac36</m>, <m>\\dfrac13</m>","s3":"<m>\\dfrac36</m>, <m>\\dfrac13</m>, <m>\\dfrac38</m>, <m>\\dfrac7{24}</m>"},
+			{"t":"Urutkan bilangan pecahan berikut dari yang terkecil<br/><m>\\dfrac45</m>, <m>\\dfrac7{10}</m>, <m>\\dfrac4{25}</m>, <m>\\dfrac7{15}</m>","b":"<m>\\dfrac4{25}</m>, <m>\\dfrac7{15}</m>, <m>\\dfrac7{10}</m>, <m>\\dfrac45</m>","s1":"<m>\\dfrac45</m>, <m>\\dfrac7{10}</m>, <m>\\dfrac7{15}</m>, <m>\\dfrac4{25}</m>","s2":"<m>\\dfrac45</m>, <m>\\dfrac4{25}</m>, <m>\\dfrac7{10}</m>, <m>\\dfrac7{15}</m>","s3":"<m>\\dfrac7{10}</m>, <m>\\dfrac7{15}</m>, <m>\\dfrac45</m>, <m>\\dfrac4{25}</m>"}]
 		 }]
 }
