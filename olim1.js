5d1a3996a680cb0a960bdc20f42c9bb4dea7b605
idhamdaz <76897779+idhamdaz@users.noreply.github.com>
2021-03-01 15:44:24 +0700

    Create olim1.js

---
+++ olim1.js
@@ -0,0 +1,23 @@
+var myObj = {
+	"soal":
+		[{"no":
+	 		[{"t":"Jika <m>{(a-b)(b-c)(c-a)=2019,}</m> maka <m>(a-b)^3+(b-c)^3+(c-a)^3=</m> ....","b":"6057","s1":"2019","s2":"4038","s3":"8076","s4":"10.095"},
+		 	{"t":"Jika <m>{(d-e)(e-f)(f-d)=2020,}</m> maka <m>(d-e)^3+(e-f)^3+(f-d)^3=</m> ....","b":"6060","s1":"2020","s2":"4040","s3":"8080","s4":"10.100"},
+			{"t":"Jika <m>{(j-k)(k-l)(l-j)=2021,}</m> maka <m>(j-k)^3+(k-l)^3+(l-j)^3=</m> ....","b":"6063","s1":"2021","s2":"4042","s3":"8084","s4":"10.105"}]
+		 },
+		{"no":
+	 		[{"t":"<m>\sqrt[3]{2+\sqrt5}+\sqrt[3]{2-\sqrt5}=</m>","b":"1","s1":"3","s2":"4","s3":"5","s4":"2"},
+		 	{"t":"<m>\sqrt[3]{9+4\sqrt5}+\sqrt[3]{9-4\sqrt5}=</m>","b":"3","s1":"1","s2":"4","s3":"5","s4":"2"},
+			{"t":"<m>\sqrt[3]{25+10\sqrt5}+\sqrt[3]{25-10\sqrt5}=</m>","b":"5","s1":"1","s2":"3","s3":"4","s4":"2"}]
+		 },
+		{"no":
+	 		[{"t":"Misalkan <m>a</m> adalah bilangan real dan <m>{\sqrt[3]a+\dfrac1{\sqrt[3]a}=1,}</m> maka <m>{a+\dfrac1a=}</m> ....","b":"<m>-2</m>","s1":"<m>-1</m>","s2":"<m>0</m>","s3":"<m>1</m>","s4":"<m>3</m>},
+		 	{"t":"Misalkan <m>b</m> adalah bilangan real dan <m>{\sqrt[3]b+\dfrac1{\sqrt[3]b}=2,}</m> maka <m>{b+\dfrac1b=}</m> ....","b":"<m>2</m>","s1":"<m>1</m>","s2":"<m>3</m>","s3":"<m>4</m>","s4":"<m>0</m>},
+			{"t":"Misalkan <m>c</m> adalah bilangan real dan <m>{\sqrt[3]c+\dfrac1{\sqrt[3]c}=3,}</m> maka <m>{b+\dfrac1b=}</m> ....","b":"<m>18</m>","s1":"<m>12</m>","s2":"<m>6</m>","s3":"<m>3</m>","s4":"<m>1</m>}]
+		 },
+		{"no":
+	 		[{"t":"Jika <m>x</m> dan <m>y adalah penyelesaian dari persamaan <m>{xy+\dfrac{x^3+y^3}=1,}</m> maka <m>{a+\dfrac1a=}</m> ....","b":"<m>-2</m>, <m>\\dfrac{11}{16}</m>, <m>\\dfrac12</m>, <m>\\dfrac3{32}</m>","s1":"<m>\\dfrac12</m>, <m>\\dfrac68</m>, <m>\\dfrac{11}{16}</m>, <m>\\dfrac3{32}</m>","s2":"<m>\\dfrac12</m>, <m>\\dfrac{11}{16}</m>, <m>\\dfrac3{32}</m>, <m>\\dfrac68</m>","s3":"<m>\\dfrac3{32}</m>, <m>\\dfrac{11}{16}</m>, <m>\\dfrac68</m>, <m>\\dfrac12</m>"},
+		 	{"t":"Urutkan bilangan pecahan berikut dari yang terkecil<br/><m>\\dfrac7{24}</m>, <m>\\dfrac36</m>, <m>\\dfrac13</m>, <m>\\dfrac38</m>","b":"<m>\\dfrac7{24}</m>, <m>\\dfrac13</m>, <m>\\dfrac38</m>, <m>\\dfrac36</m>","s1":"<m>\\dfrac13</m>, <m>\\dfrac36</m>, <m>\\dfrac38</m>, <m>\\dfrac7{24}</m>","s2":"<m>\\dfrac7{24}</m>, <m>\\dfrac38</m>, <m>\\dfrac36</m>, <m>\\dfrac13</m>","s3":"<m>\\dfrac36</m>, <m>\\dfrac13</m>, <m>\\dfrac38</m>, <m>\\dfrac7{24}</m>"},
+			{"t":"Urutkan bilangan pecahan berikut dari yang terkecil<br/><m>\\dfrac45</m>, <m>\\dfrac7{10}</m>, <m>\\dfrac4{25}</m>, <m>\\dfrac7{15}</m>","b":"<m>\\dfrac4{25}</m>, <m>\\dfrac7{15}</m>, <m>\\dfrac7{10}</m>, <m>\\dfrac45</m>","s1":"<m>\\dfrac45</m>, <m>\\dfrac7{10}</m>, <m>\\dfrac7{15}</m>, <m>\\dfrac4{25}</m>","s2":"<m>\\dfrac45</m>, <m>\\dfrac4{25}</m>, <m>\\dfrac7{10}</m>, <m>\\dfrac7{15}</m>","s3":"<m>\\dfrac7{10}</m>, <m>\\dfrac7{15}</m>, <m>\\dfrac45</m>, <m>\\dfrac4{25}</m>"}]
+		 }]
+}
